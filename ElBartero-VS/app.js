9f5f243d664733632ff6bf6cbfd0d30da1a672bb
Migueab <migueabentin@hotmail.com>
2023-05-14 19:03:03 -0300

    Actualizando archivos, Footer avanzado

--- ElBartero-VS/app.js
+++ ElBartero-VS/app.js
@@ -10,9 +10,11 @@ app.use ( express.static ( publicPath));
 
 
 app.listen ( 3002 , ()=>{
-
+    
     console.log ( 'Servidor corriendo');
 });
+
+
 
 
 app.get ('/home' , ( req , res )=>{
@@ -21,69 +23,72 @@ app.get ('/home' , ( req , res )=>{
 });
 
 
-app.get ( '/balanza' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/balanza.html'));
-
-});
-
-app.get ( '/cuentadeusuario' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/cuentaDeUsuario.html'));
-
-});
-
-app.get ( '/mercaderiaenexhibicion' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/mercaderiaExhibida.html'));
-
-});
-
-
-app.get ( '/arrepentimientodeactividades' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/foo-arrepentimiento.html'));
-
-});
-
-app.get ( '/contacto' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/foo-contacto.html'));
-
-});
-
-app.get ( '/mercadocomercial' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/mercadoComercial.html'));
-    
-});
-
-app.get ( '/mercadodeusuarios' , ( req , res ) =>{
-
-    res.sendFile ( path.join (__dirname , 'views/mercadoDeUsuarios.html'));
-    
-});
-
-
-/*  Registrarse e inicio de sesion*/
+/*  Registrarse e inicio de sesion Abre */
 
 app.get ( '/iniciosesion' , ( req , res ) =>{
-
+    
     res.sendFile ( path.join (__dirname , 'views/signIn.html'));
     
 });
 
 
 app.get ( '/registrarse' , ( req , res ) =>{
-
+    
     res.sendFile ( path.join (__dirname , 'views/signUp.html'));
     
 });
 
-/*  Registrarse e inicio de sesion */
+/*  Registrarse e inicio de sesion Cierra */
+
+
+app.get ( '/cuentadeusuario' , ( req , res ) =>{
+    
+    res.sendFile ( path.join (__dirname , 'views/cuentaDeUsuario.html'));
+    
+});
+
+
+app.get ( '/mercadocomercial' , ( req , res ) =>{
+    
+    res.sendFile ( path.join (__dirname , 'views/mercadoComercial.html'));
+    
+});
+
+app.get ( '/mercadodeusuarios' , ( req , res ) =>{
+    
+    res.sendFile ( path.join (__dirname , 'views/mercadoDeUsuarios.html'));
+    
+});
+
+/* mercaderias en exhibicion  Abre */
+
+app.get ( '/mercaderiaenexhibicion' , ( req , res ) =>{
+    
+    res.sendFile ( path.join (__dirname , 'views/mercaderiaExhibida.html'));
+    
+});
+
+
+app.get ( '/mercaderiausuarios' , ( req , res ) =>{
+    
+    res.sendFile ( path.join (__dirname , 'views/mercaderiaUsuarios.html'));
+    
+});
+
+/* mercaderias en echibicion Cierra */
+
+
+
+/*  Balanza - Subasta - valoracion - Canasta  Abre */
+
+app.get ( '/balanza' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/balanza.html'));
+
+});
 
 app.get ( '/subasta' , ( req , res ) =>{
-
+    
     res.sendFile ( path.join (__dirname , 'views/subasta.html'));
     
 });
@@ -100,6 +105,65 @@ app.get ( '/canasta' , ( req , res ) =>{
     
 });
 
+/* balanza - subasta - valoracion - canasta  Cierra */
+
+/* Footer */
+
+app.get ( '/arrepentimientodeactividades' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/foo-arrepentimiento.html'));
+
+});
+
+app.get ( '/contacto' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/foo-contacto.html'));
+
+});
+
+app.get ( '/cambiosydevoluciones' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/cambiosDevoluciones.html'));
+
+});
+
+app.get ( '/politicasdeprivacidad' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/politicasPrivacidad.html'));
+
+});
+
+app.get ( '/terminosycondiciones' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/terminosCondiciones.html'));
+
+});
+
+
+app.get ( '/preguntasfrecuentes' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/preguntasFrecuentes.html'));
+
+});
+
+app.get ( '/preguntasblog' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/preguntasBlog.html'));
+
+});
+
+
+app.get ( '/transporte' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/transporte.html'));
+
+});
+
+app.get ( '/contratacion' , ( req , res ) =>{
+
+    res.sendFile ( path.join (__dirname , 'views/contratacion.html'));
+
+});
 
 
 
